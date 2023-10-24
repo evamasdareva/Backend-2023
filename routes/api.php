7a6eea451783d69980e34900fefde2959b6b5da0
evamasdareva <evamasdareva@gmail.com>
2023-10-24 14:27:40 +0700

    Menambahkan Route dan Membuat Controller Animal

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AnimalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route untuk menampilkan semua hewan
+Route::get('animals', [AnimalController::class, "index"]);
+
+// Route untuk menambahkan hewan
+Route::post('animals', [AnimalController::class, "store"]);
+
+//Route untuk mengedit hewan
+Route::put('animals/{id}', [AnimalController::class, "update"]);
+
+// Route untuk menghapus hewan
+Route::delete('animals/{id}', [AnimalController::class, "destroy"]);
