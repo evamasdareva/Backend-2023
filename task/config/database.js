dd13b1d2fa1f379b58dd0e759e2d3175e3cc715d
evamasdareva <114887945+evamasdareva@users.noreply.github.com>
2024-01-04 11:51:23 +0700

    Pertemuan-13

--- task/config/database.js
+++ task/config/database.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 // make connection
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
+    user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
 });
--- task/routes/api.js
+++ task/routes/api.js
@@ -1,18 +1,23 @@
-// import StudentController
-const StudentController = require('../controllers/StudentController');
+// import Student Controller
+const StudentController = require("../controllers/StudentController");
 
-const express = require('express');
+// import express
+const express = require("express");
+
+// make an object router
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Welcome to Student API');
+// make home routing
+router.get("/", (req, res) => {
+    res.send("Hello Ziyad");
 });
 
-// student routes
-router.get('/students', StudentController.index);
-router.post('/students', StudentController.store);
-router.put('/students/:id', StudentController.update);
-router.delete('/students/:id', StudentController.destroy);
+// Routing for Students
+router.get("/students", StudentController.index);
+router.post("/students", StudentController.store);
+router.put("/students/:id", StudentController.update);
+router.delete("/students/:id", StudentController.destroy);
+router.get("/students/:id", StudentController.show);
 
-// export router
+// export routing
 module.exports = router;
